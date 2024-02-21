a5c0210f6f0876d66a06548afd9db647ba9bbcad
Rishivatsal Mishra <121601369+RishiVT2004@users.noreply.github.com>
2024-02-21 22:37:38 +0530

    Update web_example.py

--- flask/web_example.py
+++ flask/web_example.py
@@ -1,14 +1,14 @@
 from flask import Flask,render_template
-from flask_sqlalchemy import SQLAlchemy
 
-#creating a database and using sql-alchemy
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASES_URI'] = "sqlite:///portfolio.db" #sqlite database
-db = SQLAlchemy(app)
 
 @app.route('/')
-def e_shop():
+def prsonal_portfolio():
     return render_template('portfolio.html')
+
+@app.route('/socials')
+def social_info():
+    return render_template('socials.html')
 
 if __name__ == "__main__":
     app.run(debug="True",port=8000)
