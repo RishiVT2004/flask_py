b5162bf18f710be60e65e1975a011c1cf50f4e7a
Rishivatsal Mishra <121601369+RishiVT2004@users.noreply.github.com>
2024-02-26 23:37:37 +0530

    Add files via upload

--- flask/dynamic_url.py
+++ flask/dynamic_url.py
@@ -2,6 +2,9 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+#bulding url dynamically -; 
+
+
 
 @app.route('/')
 def home_page():
@@ -15,5 +18,22 @@ def social_page():
 def image_page():
     return render_template('img.html')
 
+@app.route('/cgpa/<float:cgpa>') #parametric values, building url dynamically...
+def calculate(cgpa): 
+    return f"cgpa of the person is "+str(cgpa)
+
+@app.route('/cgpa/<float:cgpa>/checkresult')
+def check_pass_fail(cgpa):
+    result = ""
+    if cgpa>7.5:
+        result="eligible to appear for intern"
+    else:
+        result="not-eligible to appear for intern"
+
+    return f'the person is '+str(result)        
+    
+
+
 if __name__ == "__main__": #main method -> execution starts here
     app.run(debug=True,port=8000) # debug = True -> helps to update page without re running...
+
